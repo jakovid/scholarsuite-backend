4f91359e483483826ba01a24b805acd800851783
Stephen Smith <stephenlehanesmith@gmail.com>
2023-10-31 11:43:11 +0000

    Create sign up view
    Affected files are scholarsuite/serializers.py, scholaresuite/urls.py and scholarsuite/views.py

--- scholarsuite/scholarsuite/urls.py
+++ scholarsuite/scholarsuite/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, UserRegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('register/', UserRegistrationView.as_view(), name='user-registration'),
 ]
